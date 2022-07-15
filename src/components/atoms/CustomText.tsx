a665d8fe9f0f6a71ed4e8e6d2ce58d06e3d89569
vcz-K <80806184+vcz-K@users.noreply.github.com>
2022-07-15 17:59:47 +0900

    폰트 적용

--- src/components/atoms/CustomText.tsx
+++ src/components/atoms/CustomText.tsx
@@ -1,24 +1,46 @@
 import React from "react";
 import styled from "styled-components/native";
+import { Platform } from "react-native";
 
 const handleFont = (fontWeight: string) => {
-    switch(fontWeight) {
-        case "100":
-        case "200":
-            return "Spoqa-Han-Sans-Neo-Thin";
-        case "300":
-            return "Spoqa-Han-Sans-Neo-Light";
-        case "400":
-            return "Spoqa-Han-Sans-Neo-Regular";
-        case "500":
-        case "600":
-            return "Spoqa-Han-Sans-Neo-Medium";
-        case "700":
-        case "800":
-            return "Spoqa-Han-Sans-Neo-Bold";
-        default:
-            return "Spoqa-Han-Sans-Neo-Regular";
+    if (Platform.OS === "ios") {
+        switch(fontWeight) {
+            case "100":
+            case "200":
+                return "SpoqaHanSansNeo-Thin";
+            case "300":
+                return "SpoqaHanSansNeo-Light";
+            case "400":
+                return "SpoqaHanSansNeo-Regular";
+            case "500":
+            case "600":
+                return "SpoqaHanSansNeo-Medium";
+            case "700":
+            case "800":
+                return "SpoqaHanSansNeo-Bold";
+            default:
+                return "SpoqaHanSansNeo-Regular";
+        }
+    } else {
+        switch(fontWeight) {
+            case "100":
+            case "200":
+                return "Spoqa-Han-Sans-Neo-Thin";
+            case "300":
+                return "Spoqa-Han-Sans-Neo-Light";
+            case "400":
+                return "Spoqa-Han-Sans-Neo-Regular";
+            case "500":
+            case "600":
+                return "Spoqa-Han-Sans-Neo-Medium";
+            case "700":
+            case "800":
+                return "Spoqa-Han-Sans-Neo-Bold";
+            default:
+                return "Spoqa-Han-Sans-Neo-Regular";
+        }
     }
+
 }
 
 const StyledText = styled.Text<{ fontWeight: string; }>`
